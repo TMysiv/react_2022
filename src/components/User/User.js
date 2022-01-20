8d4cf1915c80fa8679e654609b4977403acb21b2
TMysiv <tarasmysiv@gmail.com>
2022-01-20 18:43:03 +0200

    add hw_3

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -8,7 +8,10 @@ const User = ({user}) => {
 
     return (
         <div className={css.user}>
-            <Link to={id.toString()} state={user}>{id}) {name}</Link>
+            {id}) {name}
+            <Link to={id.toString()} state={user}>
+                <button>Details</button>
+            </Link>
         </div>
     );
 };
