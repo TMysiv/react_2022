af05f2576c32bccad5655b97b583aa1a99d9fbf2
TMysiv <tarasmysiv@gmail.com>
2022-02-03 21:14:47 +0200

    add hw_6

---
+++ Task_1/hw_6.ts
@@ -0,0 +1,56 @@
+import {ISpace} from "./interfaces/space.interface";
+
+const space:ISpace ={
+    mission_name:'Starlink-15 (v1.0)',
+    launch_date_local:'2020-10-24T11:31:00-04:00',
+    launch_site:{
+        site_name_long:'Cape Canaveral Air Force Station Space Launch Complex 40'
+    },
+    links:{
+        article_link:null,
+        video_link:'https://youtu.be/J442-ti-Dhg'
+    },
+    rocket:{
+        rocket_name:'Falcon 9',
+        first_stage:{
+            cores:[
+                {
+                    flight: 7,
+                    core: {
+                        reuse_count: 6,
+                        status: 'unknown'
+                    }
+                }
+            ]
+        },
+        second_stage:{
+            payloads:[
+                {
+                    payload_type:'Satellite',
+                    payload_mass_kg:15400,
+                    payload_mass_lbs:33951
+                }
+            ]
+        }
+    }
+}
+
+//протипизировать функции:
+
+import {IUser} from "../Task_1/interfaces/user.interface";
+
+const user:IUser={name:'Max',age:18,gender:'male'}
+
+////////
+
+function sum(a:number,b:number):number{
+    return a+b
+}
+function showSum(a:number,b:number):void{
+    console.log(a + b);
+}
+function incAge(someUser:IUser, inc:number):IUser{
+    someUser.age+=inc
+    return someUser
+}
+
