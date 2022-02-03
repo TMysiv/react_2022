c2ecc57557c648767b81c659344b983c48ae5364
TMysiv <tarasmysiv@gmail.com>
2022-02-03 22:00:47 +0200

    add homework_6

--- task_1.ts
+++ task_1.ts
@@ -1,37 +1,38 @@
-import {ISpace} from "./interfaces/space.interface";
-
-const space: ISpace = {
-    mission_name:'Starlink-15 (v1.0)',
-    launch_date_local:'2020-10-24T11:31:00-04:00',
-    launch_site:{
-        site_name_long:'Cape Canaveral Air Force Station Space Launch Complex 40'
-    },
-    links:{
-        article_link:null,
-        video_link:'https://youtu.be/J442-ti-Dhg'
-    },
-    rocket:{
-        rocket_name:'Falcon 9',
-        first_stage:{
-            cores:[
-                {
-                    flight:7,
-                    core:{
-                        reuse_count:6,
-                        status:'unknown'
-                    }
-                }
-            ]
+// 1) создать интерфейс на основе этого объекта:
+export interface ISpace {
+    mission_name: string,
+    launch_date_local: string,
+    launch_site: ILaunch_site,
+    links:ILink,
+    rocket: {
+        rocket_name: string,
+        first_stage: {
+            cores: [ICores]
         },
-        second_stage:{
-            payloads:[
-                {
-                    payload_type:'Satellite',
-                    payload_mass_kg:15400,
-                    payload_mass_lbs:33951
-                }
-            ]
+        second_stage: {
+            payloads: [IPay]
         }
     }
-
 }
+
+interface ILink {
+    article_link: null,
+    video_link: string
+}
+interface IPay{
+    payload_type: string,
+    payload_mass_kg: number,
+    payload_mass_lbs: number
+}
+interface ICore{
+    reuse_count: number,
+    status: string
+}
+interface ILaunch_site{
+    site_name_long: string
+}
+interface ICores{
+    flight: number,
+    core: ICore
+}
+
