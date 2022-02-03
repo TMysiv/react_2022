454b08bc6acdfcb1e771a47c790bf755de1d9f7d
TMysiv <tarasmysiv@gmail.com>
2022-02-03 21:55:58 +0200

    add homework_6

---
+++ interfaces/space.interface.ts
@@ -0,0 +1,38 @@
+// 1) создать интерфейс на основе этого объекта:
+export interface ISpace {
+    mission_name: string,
+    launch_date_local: string,
+    launch_site: ILaunch_site,
+    links:ILink,
+    rocket: {
+        rocket_name: string,
+        first_stage: {
+            cores: [ICores]
+        },
+        second_stage: {
+            payloads: [IPay]
+        }
+    }
+}
+
+interface ILink {
+    article_link: null,
+    video_link: string
+}
+interface IPay{
+    payload_type: string,
+    payload_mass_kg: number,
+    payload_mass_lbs: number
+}
+interface ICore{
+    reuse_count: number,
+    status: string
+}
+interface ILaunch_site{
+    site_name_long: string
+}
+interface ICores{
+    flight: number,
+    core: ICore
+}
+
---
+++ task_2.ts
@@ -0,0 +1,17 @@
+// 2) протипизировать функции:
+
+import {IUser} from "./interfaces/user.interface";
+
+const user:IUser = {name:'Max',age:18,gender:'male'}
+
+function sum(a:number,b:number):number{
+    return a+b
+}
+function showSum(a:number,b:number):void{
+    console.log(a + b);
+}
+
+function incAge(someUser:IUser, inc:number):IUser{
+    someUser.age+=inc
+    return someUser
+}
